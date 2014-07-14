c6a721f2bf7944186e3ff27b9c1634a1a2da6a94
Yubin Bai <ybai@thrillist.com>
2014-07-14 16:32:57 -0400

    624

---
+++ testprograms/ConundrumReloaded.java
@@ -0,0 +1,111 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+import java.awt.geom.*;
+
+public class ConundrumReloaded
+{
+	public int minimumLiars(String answers)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		long time;
+		int answer;
+		boolean errors = false;
+		int desiredAnswer;
+		
+		
+		time = System.currentTimeMillis();
+		answer = new ConundrumReloaded().minimumLiars("LLH");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 1;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new ConundrumReloaded().minimumLiars("?????");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 0;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new ConundrumReloaded().minimumLiars("LHLH?");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 2;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new ConundrumReloaded().minimumLiars("??LLLLLL??");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 3;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new ConundrumReloaded().minimumLiars("LLL");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = -1;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		
+		
+		if (errors)
+			System.out.println("Some of the test cases had errors :-(");
+		else
+			System.out.println("You're a stud (at least on the test data)! :-D ");
+	}
+
+}
+//Powered by [KawigiEdit] 2.0!

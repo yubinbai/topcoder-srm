a82a5be7998c7d4d28bf0c60bd87f754c8e4bdac
Yubin Bai <ybai@thrillist.com>
2014-07-21 17:31:54 -0400

    627

---
+++ srm627/HappyLetterDiv2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+
+
+public class HappyLetterDiv2
+{
+	public char getHappyLetter(String letters)
+	{
+		int[] freq = new int[30];
+		for (char c : letters.toCharArray()) {
+			freq[c - 'a']++;
+		}
+		int len = letters.length();
+		for (int i = 0; i < 26; ++i) {
+		   	if (freq[i] > len / 2) return (char) ('a' + i);
+		}
+		return '.';
+	}
+	
+}
+//Powered by KawigiEdit 2.1.8 (beta) modified by pivanof!

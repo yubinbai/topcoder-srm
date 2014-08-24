c3771934687773d3b6e3d35839b662c14ad4bd8e
Yubin Bai <baiyubin@gmail.com>
2014-08-24 18:55:17 -0400

    611

--- srm146/YahtzeeScore.java
+++ srm146/YahtzeeScore.java
@@ -3,11 +3,11 @@ package topcoder;
 public class YahtzeeScore {
     public int maxPoints(int[] toss) {
         int[] a = new int[7];
-        for (int e : toss){
+        for (int e : toss) {
             a[e] += e;
         }
         int res = 0;
-        for (int e : a){
+        for (int e : a) {
             res = Math.max(e, res);
         }
         return res;
--- srm608/OneDimensionalRobotEasy.java
+++ srm608/OneDimensionalRobotEasy.java
@@ -1,9 +1,7 @@
-package topcoder;
-
 public class OneDimensionalRobotEasy {
     public int finalPosition(String commands, int A, int B) {
         int curr = 0;
-        for (int i = 0;i<commands.length();i++){
+        for (int i = 0; i < commands.length(); i++) {
             if (commands.charAt(i) == 'L' && curr > A * -1)
                 curr--;
             else if (commands.charAt(i) == 'R' && curr < B)

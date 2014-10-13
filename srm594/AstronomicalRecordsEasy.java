3042f78458ae862a4dab6d50d7a3cbc4dd207418
Yubin Bai <ybai@thrillist.com>
2014-10-13 18:25:24 -0400

    Simpler

--- srm594/AstronomicalRecordsEasy.java
+++ srm594/AstronomicalRecordsEasy.java
@@ -22,27 +22,17 @@ public class AstronomicalRecordsEasy
                 for (int k = 0; k < m; k++) {
                     bb[k] = B[k] * lcm / B[j];
                 }
-                int lcs = lcs(aa, bb);
-                ret = Math.min(ret, n + m - lcs);
+                HashSet<Integer> set = new HashSet<Integer>();
+                for (int a : aa) {
+                	set.add(a);
+                }
+                for (int b : bb) {
+                	set.add(b);
+                }
+                ret = Math.min(ret, set.size()); 
             }
         }
         return ret;
-    }
-
-    private int lcs(int[] a, int[] b) {
-        int n = a.length;
-        int m = b.length;
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (a[i - 1] == b[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i -1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
     }
 
     private int lcm(int a, int b) {

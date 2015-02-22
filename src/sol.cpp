1e13cdb23bc271c6f873acbc10661f32b4c144f8
Yubin Bai <baiyubin@gmail.com>
2015-02-22 00:05:58 -0500

    600

--- src/sol.cpp
+++ src/sol.cpp
@@ -1,91 +1,89 @@
-// #include <bits/stdc++.h>
-#define  RD(x)      scanf("%d", &x)
-#define  REP(i, n)  for (int i=0; i<int(n); ++i)
-#define  FOR(i, n)  for (int i=1; i<=int(n); ++i)
-#define  pii        pair<int, int>
-#define  piL        pair<int, long long>
-#define  mp         make_pair
-#define  pb         push_back
-#define  whatis(x)  cout << #x << ": " << x << endl;
-int dx[4] = {0, -1, 0, 1};
-int dy[4] = {1, 0, -1, 0};
+#include <vector> 
+#include <list> 
+#include <map> 
+#include <set> 
+#include <deque> 
+#include <stack> 
+#include <bitset> 
+#include <algorithm> 
+#include <functional> 
+#include <numeric> 
+#include <sstream> 
+#include <iostream> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstdlib> 
+#include <ctime> 
+#include <queue> 
+#include <cstring> 
+using namespace std; 
 
-using namespace std;
-#include "vector"
-#define  N   123456
-#define  eps 1e-8
-#define  pi  acos(-1.0)
-#define  inf 0XFFFFFFFll
-#define  mod 1000000007ll
-#define  LL  long long
-#define  ULL unsigned long long
+struct PalindromeMatrixDiv2 { 
+  int minChange(vector <string> A, int rowCount, int columnCount); 
+}; 
 
-class BuildingTowers
-{
-public:
-    long long maxHeight(int _n, int _K, vector <int> x, vector <int> t)
-    {
-        LL n = _n;
-        LL K = _K;
-        vector<LL> X, T;
-        X.push_back(1);
-        T.push_back(0);
+inline int BIT(int n, int i) { return (n>>i)&1; } 
 
-        REP(i, x.size()) if (x[i] != 1)
-            X.push_back(x[i]), T.push_back(t[i]);
+void Generate(vector<int> &t, int n, int c) { 
+  t.clear(); 
+  for(int i = 0; i < (1<<n); i++) { 
+    int cnt = 0; 
+    for(int j = 0; j < n; j++) 
+      if (BIT(i, j)) cnt++; 
+    if (cnt == c) t.push_back(i); 
+  } 
+} 
 
-        if (X[X.size() - 1] != n)
-            X.push_back(n), T.push_back((n - 1)*K);
+int a[8][8]; 
+int consider[8][8], key; 
+int n, m; 
 
+void dfs(int i, int j, int r, int c, int Count[2]) { 
+  if (consider[r][c] == key) return; 
+  consider[r][c] = key; 
+  Count[a[r][c]]++; 
+  if (BIT(i, r)) dfs(i, j, r, m-c-1, Count); 
+  if (BIT(j, c)) dfs(i, j, n-r-1, c, Count); 
+} 
 
-        bool ok = true;
-        while (ok)
-        {
-            ok = false;
-            for (int i = 1; i < (int)X.size(); i++)
-            {
-                LL u = T[i - 1] + (X[i] - X[i - 1]) * K;
-                if (T[i] > u)
-                {
-                    T[i] = u;
-                    ok = true;
-                }
-            }
+int PalindromeMatrixDiv2::minChange(vector <string> A, int rowCount, int columnCount) { 
 
-            for (int i = X.size() - 2; i >= 0; i--)
-            {
-                LL u = T[i + 1] + (X[i + 1] - X[i]) * K;
-                if (T[i] > u)
-                {
-                    T[i] = u;
-                    ok = true;
-                }
-            }
-        }
+  n = A.size(); 
+  m = A[0].length(); 
+  for(int i = 0; i < n; i++) 
+    for(int j = 0; j < m; j++) 
+      a[i][j] = A[i][j]-'0'; 
 
-        LL ans = 0;
-        for (LL v : T)
-            ans = max(ans, v);
+  vector<int> tr, tc; 
+  Generate(tr, n, rowCount); 
+  Generate(tc, m, columnCount); 
 
-        for (int i = 1; i < X.size(); i++)
-        {
-            LL l = 1, r = T[i - 1] + (X[i] - X[i - 1]) * K;
-            while (l < r)
-            {
-                LL mid = (l + r + 1) / 2;
-                LL need = (mid - T[i - 1] + K - 1) / K;
-                LL all = X[i] - X[i - 1];
-                LL has = all - need;
-                if (mid - has * K <= T[i])
-                    l = mid;
-                else
-                    r = mid - 1;
-            }
-            ans = max(ans, l);
-        }
+  key = 0; 
+  memset(consider, 0, sizeof consider); 
 
-        return ans;
-    }
+  int ret = 1e9; 
+  for(int ii = 0; ii < tr.size(); ii++) { 
+    int i = tr[ii]; 
+    for(int jj = 0; jj < tc.size(); jj++) { 
+      int j = tc[jj]; 
+      int tret = 0; 
+      key++; 
+      for(int r = 0; r < n; r++) 
+        for(int c = 0; c < m; c++) { 
+          int Count[2] = {0, 0}; 
+          dfs(i, j, r, c, Count); 
+          tret += min(Count[0], Count[1]); 
+        } 
+
+      ret = min(ret, tret); 
+    } 
+  } 
+
+  return ret; 
+} 
 
 
-};
+
+// Powered by FileEdit
+// Powered by TesterDream 1.2.4 by fushar (December 19 2012)
+// Powered by CodeProcessor
